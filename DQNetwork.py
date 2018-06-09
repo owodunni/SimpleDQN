b0b18e2afded5330cd037a904d1e620038a019b7
Alexander Poole <alex.o.poole@gmail.com>
2018-06-09 16:45:28 +0200

    Implemented a simple random action agent

---
+++ DQNetwork.py
@@ -0,0 +1,16 @@
+import random
+
+
+import argparse
+import sys
+
+import gym
+from gym import wrappers, logger
+
+class RandomAgent(object):
+    """The world's simplest agent!"""
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def act(self, observation, reward, done):
+        return self.action_space.sample()
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import gym
+from DQNetwork import RandomAgent
 
 env = gym.make('Pendulum-v0')
 env.reset()
 is_done = False
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())  # take a random action
+
+myAgent = RandomAgent(env.action_space)
+
+episode_count = 1000
+reward = 0
+done = False
+
+for i in range(episode_count):
+    env.seed(55)
+    ob = env.reset()
+    while True:
+        env.render()
+        action = myAgent.act(ob, reward, done)
+        ob, reward, done, _ = env.step(action)
+        if done:
+            break
+        # Note there's no env.render() here. But the environment still can open window and
+        # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
+        # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+
+# Close the env and write monitor result info to disk
+env.close()
